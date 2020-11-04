875ac69ef18957225cef386c25a75d1f74a54e55
melo0502 <72190444+melo0502@users.noreply.github.com>
2020-11-04 21:36:48 +0900

    presence trueの記述の訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,         presence: true
-  validates :birthday,         presence: true
-  validates :email,            presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password,         presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-
+  with_options presence: true do
+    validates :nickname        
+    validates :birthday        
+    validates :email,     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password,  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
+  
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :name_sei, presence: true
-    validates :name_name, presence: true
+    validates :name_sei
+    validates :name_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
-    validates :name_sei_kana,  presence: true
-    validates :name_name_kana, presence: true
+    validates :name_sei_kana
+    validates :name_name_kana
   end
 end
