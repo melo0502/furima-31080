99cf7f97f1b625d891d457ba5f76169d23f5b841
melo0502 <72190444+melo0502@users.noreply.github.com>
2020-11-10 21:36:26 +0900

    商品出品機能記述の訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,13 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
-  validates :delivery_days_id, numericality: { other_than: 1 } 
-  validates :delivery_fee_id,  numericality: { other_than: 1 } 
-  validates :category_id,      numericality: { other_than: 1 } 
-  validates :ship_form_id,     numericality: { other_than: 1 } 
-  validates :status_id,        numericality: { other_than: 1 } 
+  with_options numericality: { other_than: 1 } do
+    validates :delivery_days_id 
+    validates :delivery_fee_id 
+    validates :category_id
+    validates :ship_form_id      
+    validates :status_id
+  end
 
   belongs_to :user
   has_one_attached :image
